76d4be1557cee4346bc7131198698cce2ddad859
Ismail Cetin <isocetin1907@gmail.com>
2023-02-27 13:25:08 +0100

    Added getters

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
-    <main>
-        <!-- heading -->
-        <header>
-          <img src="./assets/pinia-logo.svg" alt="pinia logo">
-          <h1>Pinia Tasks</h1>
-        </header>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
 
-        <!-- task list -->
-        <div class="task-list">
-          <div v-for="task in taskStore.tasks">
-            <TaskDetails :task="task" />
-          </div>
-        </div>
-    </main>
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'" >All tasks</button>
+      <button @click="filter = 'favs'" >Fav tasks</button>
+    </nav>
+
+    <!-- task list -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-else>
+      <p>You have {{ taskStore.favCount }} tasks left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div> 
+  </main>
 </template>
 
 <script>
-import TaskDetails from './components/TaskDetails.vue';
-import { useTaskStore } from './stores/TaskStore';
+import { ref } from "vue";
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
 
 export default {
   components: {
@@ -26,8 +40,11 @@ export default {
   setup() {
     const taskStore = useTaskStore();
 
+    const filter = ref('all')
+
     return {
       taskStore,
+      filter
     };
   },
 };
